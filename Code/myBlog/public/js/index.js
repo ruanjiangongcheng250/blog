cb9d10cda958d07e143395e5dbd1738da99196a7
zhangtaos <zhangtaos@yonyou.com>
2017-05-04 08:17:13 +0800

    选项卡的样式 以及图片未加载出来的问题

--- Code/myBlog/public/js/index.js
+++ Code/myBlog/public/js/index.js
@@ -3,12 +3,14 @@ $(function() {
 	//图片懒加载
     $("img.lazy").lazyload({
     	effect : "fadeIn",
-    	//threshold : 200
+    	threshold : 200
     });
     //goTop的显示以及回到顶部
     $('#goTop').backtop();
     //选项卡的切换
     $('header ul li').on('click',function(){
+    	$('header ul li a').removeClass('active');
+    	$(this).children('a').addClass('active');
     	var index = $(this).index();
     	$('section>div').addClass('hide').eq(index).removeClass('hide');
     });
