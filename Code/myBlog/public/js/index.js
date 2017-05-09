a347f35638a375dc76ad28a6ec7c036a40ddce9e
zhangtaos <zhangtaos@yonyou.com>
2017-05-09 08:34:04 +0800

    add index.html and modified index.js and index.css

--- Code/myBlog/public/js/index.js
+++ Code/myBlog/public/js/index.js
@@ -14,6 +14,30 @@ $(function() {
     	var index = $(this).index();
     	$('section>div').addClass('hide').eq(index).removeClass('hide');
     });
+    
+    //通过点击article_tag 切换选项卡
+    $('.article_tag').on('click',function(){
+    	var tag = $(this).data('tag');
+    	switch(tag) 
+    	{
+    		case 'healthcare':
+    		    $('header ul li a[data-name=healthcare]').trigger('click');
+    		break;
+    		case 'fitness':
+    		    $('header ul li a[data-name=fitness]').trigger('click');
+    		break;
+    		case 'it':
+    		    $('header ul li a[data-name=it]').trigger('click');
+    		break;
+    		case 'note':
+    		    $('header ul li a[data-name=note]').trigger('click');
+    		break;
+    		default:
+    		
+    		break;
+    	}
+    	
+    });
 });
 
 
