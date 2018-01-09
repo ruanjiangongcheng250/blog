ecce75c89388fb6d29e2c5b44d79c871d0ad65e8
张涛 <zhangtaos@yonyou.com>
2018-01-09 13:26:17 +0800

    上传sql文件 Signed-off-by: 张涛 <zhangtaos@yonyou.com>

--- Code/myBlog/public/php/index.php
+++ Code/myBlog/public/php/index.php
@@ -16,16 +16,22 @@
 				$arr = array();
                 while($row = mysql_fetch_array($result))
 				{
-					array_push($arr,array('id'=> $row['id'], 
-										  'name'=> $row['name'], 
-										  'description'=> $row['description'], 
-										  'time'=> $row['time'], 
-										  'author'=> $row['author'], 
-										  'support'=> $row['support'],
-										  'watch'=> $row['watch'],
-										  'comment'=> $row['comment'],
-										  'type'=> $row['type']
-										  ));
+					$user = mysql_query("SELECT * from user where name ='".$row['author']."'");
+					file_put_contents('d:log.txt', "SELECT * from user where name =".$row['author'],FILE_APPEND);
+					while($row_user = mysql_fetch_object($user)){
+						$avator = $row_user->avator;
+						array_push($arr,array('id'=> $row['id'], 
+											  'name'=> $row['name'], 
+											  'description'=> $row['description'], 
+											  'time'=> $row['time'], 
+											  'author'=> $row['author'], 
+											  'support'=> $row['support'],
+											  'watch'=> $row['watch'],
+											  'comment'=> $row['comment'],
+											  'type'=> $row['type'],
+											  'avator'=> $avator
+											  ));
+					}
 				}  
 				echo json_encode($arr, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT);  //以字面编码多字节 Unicode 字符，用空白字符格式化返回的数据
 				mysql_close($conn);
