02b5257df1030a8ce0105c7be46e9103c8ec79ef
zhangtaos <zhangtaos@yonyou.com>
2018-02-07 14:53:10 +0800

    修复注册时 已知bug Signed-off-by: zhangtaos <zhangtaos@yonyou.com>

--- Code/myBlog/public/php/registe.php
+++ Code/myBlog/public/php/registe.php
@@ -15,7 +15,13 @@
 				}else if(mysql_num_rows($result3)){
 					echo json_encode(array("code"=>200, "message"=>"邮箱已存在"));
 				}else{
-					mysql_query("INSERT INTO `user`(`name`, `mobile`, `password`, `mail`) VALUES ('$name','$mobile','$password','$mail')");
+					$token = rand().rand().rand();
+					setcookie('token', $token,time()+36000,'/');
+					mysql_query("INSERT INTO `user`(`name`, `mobile`, `password`, `mail`, `token`) VALUES ('$name','$mobile','$password','$mail','$token')");
+					$result = mysql_query("SELECT * FROM user WHERE name ='".$name."'");
+					while($row = mysql_fetch_object($result)){
+						setcookie('author_id', $row->id, time()+3600, '/');
+					}
 					setcookie('name', $name, time()+3600, '/');
 					echo json_encode(array("code"=>200, "url"=>"index.html"));
 				    exit;
